840a002c7d5bfdfbae91cecb6424ef10f10f61fe
Alok <optimisticclown098@gmail.com>
2026-03-11 15:25:45 +0530

    OCP

---
+++ Solid/OCP/OCP_followed.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Product{
+public:
+    string name;
+    double price;
+
+    Product(string name, double price){
+        this->name = name;
+        this->price = price;
+    }
+};
+
+//ShoppingCart class only repsonsible for cart related logic
+class ShoppingCart{
+private:
+    vector<Product> products;
+
+public:
+    void addProducts(Product p){
+        products.push_back(p);
+    }
+
+    vector<Product> getProducts(){
+        return products;
+    }
+
+    double calculateTotal(){
+        double total = 0;
+        for(auto p: products)
+        total += p.price;
+
+        return total;
+    }
+};
+
+//only responsible for printing invoice
+class ShoppingCartPrinter{
+private:
+    ShoppingCart cart;
+
+public:
+    ShoppingCartPrinter(ShoppingCart cart){
+        this->cart = cart;
+    }
+
+    void printInvoice(){
+        cout<< "Shopping cart invoice:"<<endl;
+        for(auto p: cart.getProducts()){
+             cout << p.name << " - $" << p.price << endl;
+        }
+        cout << "Total: $" << cart.calculateTotal() << endl;
+
+    }
+};
+
+//only resposnible for storing to database
+class Persistence{
+public:
+    virtual void save(ShoppingCart cart) = 0;
+};
+
+class MongoPersistence : public Persistence{
+public:
+    void save(ShoppingCart cart) override{
+        cout << "Saving cart to Mongo" << endl;
+    }
+
+};
+
+class SQLPersistence : public Persistence{
+public:
+    void save(ShoppingCart cart) override{
+        cout << "Saving cart to SQL" << endl;
+    }
+};
+
+class RedisPersistence : public Persistence{
+public:
+    void save(ShoppingCart cart) override{
+        cout << "Saving cart to Redis" << endl;
+    }
+};
+
+int main(){
+
+    ShoppingCart cart;
+    cart.addProducts(Product("Antimatter", 10056000000));
+    cart.addProducts(Product("Plutonium", 100490000));
+
+    ShoppingCartPrinter printer(cart);
+    printer.printInvoice();
+
+    Persistence* SQL = new SQLPersistence;
+    Persistence* Mongo = new MongoPersistence;
+    Persistence* Redis = new RedisPersistence;
+
+    SQL->save(cart);
+    Mongo->save(cart);
+    Redis->save(cart);
+    return 0;
+}
---
+++ Solid/OCP/OCP_violated.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Product{
+public:
+    string name;
+    double price;
+
+    Product(string name, double price){
+        this->name = name;
+        this->price = price;
+    }
+};
+
+//ShoppingCart class only repsonsible for cart related logic
+class ShoppingCart{
+private:
+    vector<Product> products;
+
+public:
+    void addProducts(Product p){
+        products.push_back(p);
+    }
+
+    vector<Product> getProducts(){
+        return products;
+    }
+
+    double calculateTotal(){
+        double total = 0;
+        for(auto p: products)
+        total += p.price;
+
+        return total;
+    }
+};
+
+//only responsible for printing invoice
+class ShoppingCartPrinter{
+private:
+    ShoppingCart cart;
+
+public:
+    ShoppingCartPrinter(ShoppingCart cart){
+        this->cart = cart;
+    }
+
+    void printInvoice(){
+        cout<< "Shopping cart invoice:"<<endl;
+        for(auto p: cart.getProducts()){
+             cout << p.name << " - $" << p.price << endl;
+        }
+        cout << "Total: $" << cart.calculateTotal() << endl;
+
+    }
+};
+
+//only resposnible for storing to database
+class ShoppingCartStorage{
+private:
+    ShoppingCart cart;
+
+public:
+    ShoppingCartStorage(ShoppingCart cart){
+        this->cart = cart;
+    }
+
+    void saveToSQLDatabse(){
+        cout << "Saving shopping cart to SQL database..." << endl;
+    }
+
+    void saveToMongoDatabse(){
+        cout << "Saving shopping cart to Mongo database..." << endl;
+    }
+
+    void saveToRedisDatabse(){
+        cout << "Saving shopping cart to Redis database..." << endl;
+    }
+};
+
+int main(){
+
+    ShoppingCart cart;
+    cart.addProducts(Product("Antimatter", 10056000000));
+    cart.addProducts(Product("Plutonium", 100490000));
+
+    ShoppingCartPrinter printer(cart);
+    printer.printInvoice();
+
+    ShoppingCartStorage db(cart);
+    db.saveToSQLDatabse();
+    db.saveToRedisDatabse();
+    db.saveToMongoDatabse();
+
+    return 0;
+}
